543fe7f3034083f481691988eb45a3f292268562
haoran-data <69137565+etenix@users.noreply.github.com>
2026-02-07 21:49:29 +0900

    更新 app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, jsonify
+import logging
 
 app = Flask(__name__)
+
+# Log setting
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
 @app.route("/hello", methods=["POST"])
 def hello():
     data = request.get_json()
     name = data.get("name", "Guest")
+
+    app.logger.info(f"Request received: name={name}")
 
     return jsonify({
         "message": f"Hello {name}"
